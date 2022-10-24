8533658f831b7449bd21ded63d19a0b4933d0d56
Abeny <aben.jacob3@gmail.com>
2022-10-24 11:54:04 +0300

    modefied initiator

--- initiator/initiator.go
+++ initiator/initiator.go
@@ -1,16 +1,33 @@
 package initiator
 
 import (
-	"github.com/gravitational/trace"
-	log "github.com/sirupsen/logrus"
-	"go.uber.org/zap"
+	"github.com/swaggo/swag/example/basic/docs"
 	"net/http"
 	"os"
 	"sms-gateway/initiator/utils"
+	"sms-gateway/internal/adapter/http/middleware"
+
+	"github.com/gravitational/trace"
+	log "github.com/sirupsen/logrus"
+	"go.uber.org/zap"
 
 	//docs "crbt/cmd/rest/docs"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+// @BasePath /api/v1
+
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description do ping
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} Helloworld
+// @Router /example/helloworld [get]
 
 const (
 	authModel = "config/rbac_with_domain_model.conf"
@@ -39,14 +56,22 @@ func Initialize() {
 	if err != nil {
 		log.Fatal(er)
 	}
+	//casbin initialization
+	log.Info("initializing casbin")
+	enforcer := InitEnforcer(authModel, common.Conn)
 
 	router := gin.Default()
-
+	docs.SwaggerInfo.BasePath = "/api/v1"
 	routes := router.Group("/v1")
+	routes.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	// mw := middleware(common)
+	routes.Use(middleware.ErrorHandling())
+	// routes.Use(middleware.NewAuthorizer(logger))
 
 	// Register domains start
 	UserDomainInit(routes, common)
-
+	ClientDomainInit(enforcer, routes, common, logger)
+	//SubscriptionDomainInit(routes, common)
 	log.WithFields(log.Fields{
 		"host": os.Getenv("SERVER_HOST"),
 	}).Info("Starts Serving on HTTP")
